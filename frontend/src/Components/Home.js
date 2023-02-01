223e7b975e04830958846b4e8938af71e4fbe8cb
Rajit <73546886+rajitk13@users.noreply.github.com>
2023-02-02 01:34:50 +0530

    Landing Page Look improved

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -3,29 +3,30 @@ import classes from "./Home.module.css";
 import Button from "react-bootstrap/Button";
 import HomeTable from "./HomeTable";
 import HomeCard from "./HomeCard";
+import SVG from "./SVG";
 
 const Home = () => {
   return (
     <>
-      <div className={`${classes.hero} ${classes.spacing}`}>
-        <h1 className={classes.padleft}>
-          Looking for a Ride Partner?
-        </h1>
+      <SVG />
+      <div className={`${classes.spacing}`}>
+        <h1 className={classes.padleft}>Looking for a Ride Partner?</h1>
         <h2 className={classes.left}>MUJ to Railway Station?</h2>
         <h2 className={classes.left}>MUJ to Jaipur City ?</h2>
         <h2 className={classes.left}>MUJ to Airport?</h2>
-        <hr class="my-4" ></hr>
+        <hr class="my-4"></hr>
         <h4 className={classes.subtitle}>
           We got you covered , a Ride Sharing Solution specifically built for
           MUJ students by MUJ Students!
         </h4>
-    
-      <center>
-        <Button variant="dark" className={classes.explore} href="/explore">
-          Explore
-        </Button>
-      </center>
+
+        <center>
+          <Button variant="dark" className={classes.explore} href="/explore">
+            Explore
+          </Button>
+        </center>
       </div>
+
       <hr />
       <HomeTable css={classes} />
       <hr />
