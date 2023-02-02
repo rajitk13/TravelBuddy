73a398510776b10f5ee7ea0c2d6c06e34bb63d34
Milind <milibhatnagar2002@gmail.com>
2023-02-03 00:07:30 +0530

    Cors error

--- frontend/src/Components/login/Login.js
+++ frontend/src/Components/login/Login.js
@@ -30,9 +30,11 @@ function Login() {
       }
       const response = await fetch("https://travel-buddy-9f75.onrender.com/users/login", {
         method: "POST",
+        mode:'cors',
         body: JSON.stringify({ identification, password }),
         headers: {
           "Content-Type": "application/json",
+          "Origin":"https://travel-buddy-frontend.onrender.com/"
         },
       });
       const data = await response.json();
