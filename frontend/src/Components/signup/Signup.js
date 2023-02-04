b5777208e1b38a7b3304d9d34597057e48636269
Rajit <73546886+rajitk13@users.noreply.github.com>
2023-02-04 21:32:25 +0530

    Signup Component Update

--- frontend/src/Components/signup/Signup.js
+++ frontend/src/Components/signup/Signup.js
@@ -12,15 +12,18 @@ function Signup() {
   const phoneRef = useRef();
   const passwordRef = useRef();
   const cnfPasswordRef = useRef();
-  const [error,setError] = useState();
+  const [error, setError] = useState();
   const navigate = useNavigate();
   const [show, setShow] = useState();
   const handleClose = () => {
     setShow(false);
     navigate("/login");
   };
+
+  const [load, setLoading] = useState(false);
   async function submitHandler(e) {
     e.preventDefault();
+    setLoading(true);
     const reg = regRef.current.value;
     const name = nameRef.current.value;
     const email = emailRef.current.value;
@@ -28,27 +31,31 @@ function Signup() {
     const cnfPassword = cnfPasswordRef.current.value;
     const phone = phoneRef.current.value;
 
-    if(cnfPassword !== password ){
+    if (cnfPassword !== password) {
       setError("Password and confirm password does not match 🙄");
       return;
     }
     try {
-      const response = await fetch("http://localhost:4000/users",{
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json"
+      const response = await fetch("http://localhost:4000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body:JSON.stringify({
-          name,password,email,identification:reg,phone
-        })
+        body: JSON.stringify({
+          name,
+          password,
+          email,
+          identification: reg,
+          phone,
+        }),
       });
       const data = await response.json();
-      if(data.error){
+      if (data.error) {
         return console.log("There was error fetching database");
       }
+      setLoading(false);
       setShow(true);
       console.log(data);
-      
     } catch (error) {
       console.log(error);
     }
@@ -62,48 +69,51 @@ function Signup() {
         body="Your Account was successfully created 😃"
       />
       <Form className={classes.padding} onSubmit={submitHandler}>
-      <h1>Signup</h1>
-      <Form.Group className="mb-3">
-        <Form.Label>Registration Number</Form.Label>
-        <Form.Control type="integer" ref={regRef} />
-        <Form.Text className="text-muted">
-          Enter your MUJ Registration Number
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="integer" ref={nameRef} />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="integer" ref={emailRef} />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Mobile</Form.Label>
-        <Form.Control type="integer" ref={phoneRef} />
-      </Form.Group>
+        <h1>Signup</h1>
+        <Form.Group className="mb-3">
+          <Form.Label>Registration Number</Form.Label>
+          <Form.Control type="integer" ref={regRef} />
+          <Form.Text className="text-muted">
+            Enter your MUJ Registration Number
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="integer" ref={nameRef} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="integer" ref={emailRef} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Mobile</Form.Label>
+          <Form.Control type="integer" ref={phoneRef} />
+        </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" ref={passwordRef} />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Confirm Password</Form.Label>
-        <Form.Control type="password" ref={cnfPasswordRef} />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      <p>
-        <Form.Text className="text-muted">
-          Already a user ? <a href="/login">Login here</a>
-        </Form.Text>
-      </p>
-      {error && <p style={{color:"red", fontSize:"20px"}}>{error}</p>}
-    </Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" ref={passwordRef} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" ref={cnfPasswordRef} />
+        </Form.Group>
+        {/* <Form.Group className="mb-3">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group> */}
+        <Button variant="dark" type="submit">
+          {load && (
+            <div class="spinner-border spinner-border-sm" role="status" />
+          )}{" "}
+          Submit
+        </Button>
+        <p>
+          <Form.Text className="text-muted">
+            Already a user ? <a href="/login">Login here</a>
+          </Form.Text>
+        </p>
+        {error && <p style={{ color: "red", fontSize: "20px" }}>{error}</p>}
+      </Form>
     </>
   );
 }
